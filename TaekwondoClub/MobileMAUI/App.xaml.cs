8c909bf35771b3e122ea55e0978d8be5d99dd59e
Patryk <patryk.gielcz@gmail.com>
2023-10-22 19:53:23 +0200

    start of taekwondo diploma solution - 3 template projects created

---
+++ TaekwondoClub/MobileMAUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MobileMAUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
