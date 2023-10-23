81da2307b1490fcfa953295579a071a4e38812ae
Patryk <patryk.gielcz@gmail.com>
2023-10-23 20:09:00 +0200

    desktop project type fixed, db is connected

---
+++ TaekwondoClub/DesktopWPF/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DB;
+using Microsoft.EntityFrameworkCore;
+
+namespace DesktopWPF;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<DataContext>();
+        optionsBuilder.UseSqlServer(ConfigurationManager.ConnectionStrings["pogconnectionstring2"].ConnectionString);
+        var dbContext = new DataContext(optionsBuilder.Options);
+        dbContext.Customers.Load();
+        var data = dbContext.Customers.Local.ToObservableCollection();
+        this.DataContext = data;
+
+        InitializeComponent();
+    }
+}
