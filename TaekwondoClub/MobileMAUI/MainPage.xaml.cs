dcdd4ad4118356132419c5e475bbb69b0feeab88
Patryk <patryk.gielcz@gmail.com>
2023-11-07 18:59:39 +0100

    initial progress in mobile app -flyout menu

--- TaekwondoClub/MobileMAUI/MainPage.xaml.cs
+++ TaekwondoClub/MobileMAUI/MainPage.xaml.cs
@@ -1,30 +1,16 @@
-﻿namespace MobileMAUI
+﻿using MobileMAUI.Pages;
+
+namespace MobileMAUI;
+
+public partial class MainPage : ContentPage
 {
-    public partial class MainPage : ContentPage
+    public RestService RestService { get; set; }
+
+    public MainPage()
     {
-        int count = 0;
-
-        public MainPage()
-        {
-            InitializeComponent();
-        }
-
-        private async void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
-
-            if(count == 3) 
-            {
-                var service = new RestService();
-                await service.FetchDataContext();
-            }
-        }
+        RestService = new RestService();
+        RestService.FetchDataContext();
+        
+        InitializeComponent();
     }
 }
---
+++ TaekwondoClub/MobileMAUI/Pages/UpcomingEventsPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace MobileMAUI.Pages;
+
+public partial class UpcomingEventsPage : ContentPage
+{
+	public UpcomingEventsPage()
+	{
+		InitializeComponent();
+	}
+}
